4cd732892d46449467f8f26c3078ce81e990de63
vidapogosoft <vidapogosoft@hotmail.com>
2021-09-03 21:34:51 -0500

    Talleres y videos dia 3
    triggers y procedures

--- TallerDia3.sql
+++ TallerDia3.sql
@@ -124,3 +124,180 @@ order by b.BusinessEntityID
 select * from AdventureWorks2014.Sales.SalesOrderHeader
 select * from AdventureWorks2014.Sales.Customer
 
+select * from AdventureWorks2014.Sales.SalesOrderHeader a
+inner join AdventureWorks2014.Sales.Customer b on b.CustomerID = a.CustomerID
+where a.CustomerID = 11657
+
+
+select a.CustomerID, COUNT(*) Contador from AdventureWorks2014.Sales.SalesOrderHeader a
+inner join AdventureWorks2014.Sales.Customer b on b.CustomerID = a.CustomerID
+where a.CustomerID = 11657
+group by a.CustomerID
+
+
+
+select a.CustomerID, sum(SubTotal) as Montos from AdventureWorks2014.Sales.SalesOrderHeader a
+inner join AdventureWorks2014.Sales.Customer b on b.CustomerID = a.CustomerID
+where a.CustomerID = 11657
+group by a.CustomerID
+
+
+select a.CustomerID, COUNT(*) Contador from AdventureWorks2014.Sales.SalesOrderHeader a
+inner join AdventureWorks2014.Sales.Customer b on b.CustomerID = a.CustomerID
+group by a.CustomerID
+
+
+select a.CustomerID, YEAR(OrderDate) AnioCompra  ,COUNT(*) Contador from AdventureWorks2014.Sales.SalesOrderHeader a
+inner join AdventureWorks2014.Sales.Customer b on b.CustomerID = a.CustomerID
+group by a.CustomerID,YEAR(OrderDate)
+
+
+select a.CustomerID, YEAR(OrderDate) AnioCompra , COUNT(*) Contador from AdventureWorks2014.Sales.SalesOrderHeader a
+inner join AdventureWorks2014.Sales.Customer b on b.CustomerID = a.CustomerID
+group by a.CustomerID,YEAR(OrderDate)
+Having COUNT(*) > 5
+
+
+select a.CustomerID, YEAR(OrderDate) AnioCompra , sum(SubTotal) as Montos from AdventureWorks2014.Sales.SalesOrderHeader a
+inner join AdventureWorks2014.Sales.Customer b on b.CustomerID = a.CustomerID
+group by a.CustomerID,YEAR(OrderDate)
+Having COUNT(*) > 5
+order by sum(SubTotal) desc
+
+
+
+select a.CustomerID, YEAR(OrderDate) AnioCompra , AVG(SubTotal) as Montos from AdventureWorks2014.Sales.SalesOrderHeader a
+inner join AdventureWorks2014.Sales.Customer b on b.CustomerID = a.CustomerID
+group by a.CustomerID,YEAR(OrderDate)
+Having COUNT(*) > 5
+order by AVG(SubTotal) desc
+
+
+---- update con combinaciones
+
+select * from AdventureWorks2014.Sales.SalesOrderHeader a
+inner join AdventureWorks2014.Sales.Customer b on b.CustomerID = a.CustomerID
+where a.CustomerID = 11657
+
+
+update a
+set a.Comment = 'TALLER DE SQL SERVER'
+from AdventureWorks2014.Sales.SalesOrderHeader a
+inner join AdventureWorks2014.Sales.Customer b on b.CustomerID = a.CustomerID
+where a.CustomerID = 11657
+
+
+begin tran v
+
+DELETE a
+from AdventureWorks2014.Sales.SalesOrderHeader a
+inner join AdventureWorks2014.Sales.Customer b on b.CustomerID = a.CustomerID
+where a.CustomerID = 11657
+
+select * from AdventureWorks2014.Sales.SalesOrderHeader a
+inner join AdventureWorks2014.Sales.Customer b on b.CustomerID = a.CustomerID
+where a.CustomerID = 11657
+
+rollback tran v
+
+
+---Procedures en SQL Server
+
+select * from MovieStore..Empresa
+
+use MovieStore
+go
+alter procedure InsEmpresa
+@NumeroIdentificacion varchar(max),
+@Nombre varchar(100)
+as
+begin
+
+	DECLARE @ErrorMensaje varchar(max),  @ErrorEstado int,  @ErrorSeveridad int
+	
+	begin try
+		
+		begin transaction emp
+
+		if not exists (select 1 from MovieStore..Empresa where NumeroIdentificacion = @NumeroIdentificacion  )
+		begin
+		
+			insert into Empresa (NumeroIdentificacion, Nombre, FechaRegistro,Fecha,Hora)
+			values (@NumeroIdentificacion, @Nombre, getdate(), getdate(), getdate())
+			
+		end
+		else
+		begin
+			Print 'Regla de Negocio infringida: Registro ya existe'
+		end
+
+		if (XACT_STATE()) = 1
+		begin
+			commit transaction emp
+		end
+
+	end try
+	begin catch
+
+		select @ErrorMensaje = ERROR_MESSAGE()
+		Select @ErrorEstado = ERROR_STATE()
+		Select @ErrorSeveridad = ERROR_SEVERITY()
+
+		if (XACT_STATE()) = -1
+		begin
+			rollback transaction emp
+		end
+
+		RAISERROR (@ErrorMensaje, @ErrorSeveridad, @ErrorEstado)
+
+	end catch
+	
+end
+go
+
+
+exec InsEmpresa '0919172551002', 'Victor Portugal - trg'
+
+select * from Empresa
+
+delete from Empresa where IdEmpresa = 5
+
+
+--Bienvenido: 8-Victor Portugal - trg
+
+--registro borrado de la base de datos: 5-Victor Portugal
+
+----Triggers
+
+use MovieStore
+go
+
+Create Trigger RegistroEmpresa
+on Empresa
+for insert
+as
+begin
+		
+		declare @IdEmpresa int, @Nombre varchar(100)
+
+		select @IdEmpresa = IdEmpresa, @Nombre = Nombre from inserted
+
+		Print  concat( 'Bienvenido:', space(1), @IdEmpresa, '-',  @Nombre)
+end
+go
+
+
+Create Trigger DeleteEmpresa
+on Empresa
+for Delete
+as
+begin
+		
+		declare @IdEmpresa int, @Nombre varchar(100)
+
+		select @IdEmpresa = IdEmpresa, @Nombre = Nombre from deleted
+
+		Print  concat( 'registro borrado de la base de datos:', space(1), @IdEmpresa, '-',  @Nombre)
+end
+go
+
